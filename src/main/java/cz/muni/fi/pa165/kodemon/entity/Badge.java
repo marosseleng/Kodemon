193c510ad50b0189449f14becfd3b94590455388
Maroš Šeleng <marosseleng@gmail.com>
2016-10-19 16:50:50 +0200

    Add skeletons for classes.

---
+++ src/main/java/cz/muni/fi/pa165/kodemon/entity/Badge.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.kodemon.entity;
+
+/**
+ * Created by mseleng on 10/19/16.
+ */
+public class Badge {
+}
---
+++ src/main/java/cz/muni/fi/pa165/kodemon/enums/PokemonType.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.kodemon.enums;
+
+/**
+ * Created by mseleng on 10/19/16.
+ */
+public enum PokemonType {
+}
