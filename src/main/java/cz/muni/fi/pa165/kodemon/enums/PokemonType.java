ce713abd06f6326bae1af3e38a38c146a9704279
Matej Poklemba <matejpoklemba@gmail.com>
2016-10-23 13:59:57 +0200

    Pokemon entity and dao

--- src/main/java/cz/muni/fi/pa165/kodemon/enums/PokemonType.java
+++ src/main/java/cz/muni/fi/pa165/kodemon/enums/PokemonType.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pa165.kodemon.enums;
 
 /**
  * Created by mseleng on 10/19/16.
+ * Filled out by matejos on 10/23/16.
  */
 public enum PokemonType {
+    WATER, FIRE, EARTH, DRAGON, PSYCHIC, BUG, NORMAL, ELECTRIC, FIGHTING, POISON, GROUND, FLYING, ROCK, GHOST, FAIRY, ICE
 }
