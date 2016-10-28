5ab1210db4ed988b84ce2caac291d3e210a20675
Maroš Šeleng <marosseleng@gmail.com>
2016-10-28 16:37:33 +0200

    First bunch of tests for Gym

--- src/test/java/cz/muni/fi/pa165/GymDaoTest.java
+++ src/test/java/cz/muni/fi/pa165/GymDaoTest.java
@@ -2,10 +2,21 @@ package cz.muni.fi.pa165;
 
 import cz.muni.fi.pa165.kodemon.KodemonApplicationContext;
 import cz.muni.fi.pa165.kodemon.dao.GymDao;
+import cz.muni.fi.pa165.kodemon.dao.TrainerDao;
+import cz.muni.fi.pa165.kodemon.entity.Gym;
+import cz.muni.fi.pa165.kodemon.entity.Trainer;
+import cz.muni.fi.pa165.kodemon.enums.PokemonType;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import javax.inject.Inject;
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Created by mseleng on 10/25/16.
@@ -14,7 +25,56 @@ import javax.inject.Inject;
 public class GymDaoTest extends AbstractTestNGSpringContextTests {
 
     @Inject
-    private GymDao dao;
+    private GymDao gymDao;
 
+    @Inject
+    private TrainerDao trainerDao;
 
+    private Trainer trainer;
+
+    @BeforeMethod
+    void prepare() {
+        assertThat(gymDao, is(notNullValue(GymDao.class)));
+        gymDao.deleteAll();
+        assertThat("gymDao.count() != 0L", gymDao.count(), is(0L));
+        assertThat(trainerDao, is(notNullValue(TrainerDao.class)));
+        trainerDao.deleteAll();
+        assertThat("trainerDao.count() != 0L", trainerDao.count(), is(0L));
+        prepareTrainer();
+        assertThat("trainer == null", trainer, is(notNullValue()));
+        trainerDao.save(trainer);
+    }
+
+    @Test(expectedExceptions = {DataAccessException.class})
+    void testSaveNullGym() {
+        Gym gym = null;
+        assertThat(gym, is(nullValue(Gym.class)));
+        gymDao.save(gym);
+    }
+
+    @Test(expectedExceptions = {DataAccessException.class})
+    void testDeleteNullGym() {
+        Gym gym = null;
+        assertThat(gym, is(nullValue(Gym.class)));
+        gymDao.delete(gym);
+    }
+
+    @Test
+    void testSaveCorrectInstance() {
+        Gym gym = new Gym(trainer);
+        gym.setCity("Thunder city");
+        gym.setType(PokemonType.EARTH);
+        gymDao.save(gym);
+        assertThat("gym.getId() == null", gym.getId(), is(notNullValue(Long.class)));
+        Gym testGym = gymDao.findOne(gym.getId());
+        assertThat(gym, equalTo(testGym));
+    }
+
+    private void prepareTrainer() {
+        trainer = new Trainer();
+        trainer.setFirstName("Ash");
+        trainer.setLastName("Ketchum");
+        // 22.06.1994
+        trainer.setDateOfBirth(new Date(772293652000L));
+    }
 }
