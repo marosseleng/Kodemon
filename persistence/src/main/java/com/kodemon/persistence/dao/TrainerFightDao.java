c4a09315280941499a8775e212ea3e5eec2128ad
Maroš Šeleng <marosseleng@gmail.com>
2016-11-23 23:10:47 +0100

    Fix #35

--- persistence/src/main/java/com/kodemon/persistence/dao/TrainerFightDao.java
+++ persistence/src/main/java/com/kodemon/persistence/dao/TrainerFightDao.java
@@ -4,6 +4,7 @@ import com.kodemon.persistence.entity.Gym;
 import com.kodemon.persistence.entity.Trainer;
 import com.kodemon.persistence.entity.TrainerFight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  *
  * @author Oliver Roch
  */
+@Repository
 public interface TrainerFightDao extends JpaRepository<TrainerFight, Long> {
     /**
      * Find all fights where challenger is specified trainer
