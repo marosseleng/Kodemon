22c57c0610e9ffc21c66e9869071987ef57ff7b6
Maroš Šeleng <marosseleng@gmail.com>
2016-11-24 23:51:57 +0100

    Add some tests

---
+++ api/src/main/java/com/kodemon/api/dto/BadgeDTO.java
@@ -0,0 +1,7 @@
+package com.kodemon.api.dto;
+
+/**
+ * Created by mseleng on 11/24/16.
+ */
+public class BadgeDTO {
+}
--- service/src/main/java/com/kodemon/service/config/ServiceConfig.java
+++ service/src/main/java/com/kodemon/service/config/ServiceConfig.java
@@ -4,10 +4,14 @@ import com.kodemon.persistence.config.PersistenceConfig;
 import com.kodemon.service.implementations.BadgeServiceImpl;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Spring configuration for the service module
@@ -21,6 +25,20 @@ public class ServiceConfig {
 
     @Bean
     public Mapper dozerMapper() {
-        return new DozerBeanMapper();
+        List<String> mappings = new ArrayList<>();
+        mappings.add("dozer-config.xml");
+        return new DozerBeanMapper(mappings);
+    }
+
+    /**
+     * Custom config for Dozer if needed
+     * @author nguyen
+     *
+     */
+    public class DozerCustomConfig extends BeanMappingBuilder {
+        @Override
+        protected void configure() {
+//            mapping(Pok.class, CategoryDTO.class);
+        }
     }
 }
