2e4dc4a92093955537599e4ebccb43981f461236
Oliver Roch <wowflexor@gmail.com>
2016-11-24 21:25:08 +0100

    Score pair test

---
+++ service/src/test/java/com/kodemon/service/test/PokemonFightServiceTest.java
@@ -0,0 +1,67 @@
+package com.kodemon.service.test;
+
+import com.kodemon.persistence.entity.Pokemon;
+import com.kodemon.persistence.enums.PokemonName;
+import com.kodemon.service.config.ServiceConfig;
+import com.kodemon.service.interfaces.PokemonFightAdvantageService;
+import com.kodemon.service.interfaces.PokemonFightService;
+import com.kodemon.service.util.Pair;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Oliver Roch
+ */
+
+@ContextConfiguration(classes = ServiceConfig.class)
+public class PokemonFightServiceTest extends AbstractTestNGSpringContextTests {
+    @Mock
+    private PokemonFightAdvantageService pokemonFightAdvantageService;
+
+    @Inject
+    @InjectMocks
+    private PokemonFightService pokemonFightService;
+
+    private Pokemon challenger;
+    private Pokemon target;
+
+    @BeforeMethod
+    public void initPokemons() {
+        challenger = new Pokemon();
+        target = new Pokemon();
+
+        challenger.setLevel(10);
+        challenger.setName(PokemonName.ARTICUNO);
+        challenger.setNickname("Arti");
+
+        target.setLevel(6);
+        target.setName(PokemonName.CHARIZARD);
+        target.setNickname("Redflame");
+    }
+
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getScorePairTest() {
+        when(pokemonFightAdvantageService.computePokemonFightAdvantage(PokemonName.ARTICUNO, PokemonName.CHARIZARD)).thenReturn(1.0);
+        Pair<Double, Double> score = pokemonFightService.getScorePair(challenger, target);
+
+        Assert.assertEquals(10.0, score.getX());
+        Assert.assertEquals(6.0, score.getY());
+    }
+}
