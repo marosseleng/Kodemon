dfde83cb73058d124dabd7d83c8741fee6b1f2b4
Oliver Roch <wowflexor@gmail.com>
2016-11-24 20:14:05 +0100

    Test for fight advantage

---
+++ service/src/test/java/com/kodemon/service/test/PokemonFightAdvantageServiceTest.java
@@ -0,0 +1,37 @@
+package com.kodemon.service.test;
+
+import com.kodemon.persistence.enums.PokemonName;
+import com.kodemon.service.config.ServiceConfig;
+import com.kodemon.service.interfaces.PokemonFightAdvantageService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+/**
+ * @author Oliver Roch
+ */
+
+@ContextConfiguration(classes = ServiceConfig.class)
+public class PokemonFightAdvantageServiceTest extends AbstractTestNGSpringContextTests {
+    @Inject
+    private PokemonFightAdvantageService pokemonFightAdvantageService;
+
+    private PokemonName challenger;
+    private PokemonName target;
+
+    @BeforeMethod
+    public void initPokemonNames() {
+        challenger = PokemonName.ARTICUNO;
+        target = PokemonName.CHARIZARD;
+    }
+
+    @Test
+    public void computePokemonFightAdvantageTest() {
+        double pokemonFightAdvantage = pokemonFightAdvantageService.computePokemonFightAdvantage(challenger, target);
+        Assert.assertEquals(1.0,pokemonFightAdvantage);
+    }
+}
