3d079818bf27b1c53ac947b23662dc8a104d5694
Maroš Šeleng <marosseleng@gmail.com>
2016-11-25 21:26:31 +0100

    add some tests

--- service/src/test/java/com/kodemon/service/test/PokemonServiceTest.java
+++ service/src/test/java/com/kodemon/service/test/PokemonServiceTest.java
@@ -9,22 +9,23 @@ import com.kodemon.service.interfaces.PokemonService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
-
 import java.util.Calendar;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by mseleng on 11/25/16.
  */
 @ContextConfiguration(classes = ServiceConfig.class)
-public class PokemonServiceTest {
+public class PokemonServiceTest extends AbstractTestNGSpringContextTests {
 
     @Mock
     private PokemonDao dao;
@@ -34,6 +35,7 @@ public class PokemonServiceTest {
     private PokemonService service;
 
     private Trainer trainer;
+    private Pokemon pokemon;
 
     @BeforeClass
     void setUp() {
@@ -43,15 +45,59 @@ public class PokemonServiceTest {
     @Test
     void testSaveCorrectInstance() {
         prepareTrainer();
-
-        Pokemon pokemon = new Pokemon(PokemonName.SHELLDER);
-        pokemon.setLevel(13);
-        pokemon.setTrainer(trainer);
-        pokemon.setNickname("Shelly");
+        preparePokemon();
 
         when(dao.save(pokemon)).thenReturn(pokemon);
         service.save(pokemon);
         verify(dao).save(pokemon);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    void testSaveNullInstance() {
+        when(dao.save((Pokemon) null)).thenThrow(NullPointerException.class);
+        service.save(null);
+    }
+
+    @Test
+    void testDeleteExistingInstance() {
+        prepareTrainer();
+        preparePokemon();
+
+        doNothing().when(dao).delete(pokemon);
+        service.delete(pokemon);
+        verify(dao).delete(pokemon);
+    }
+
+    @Test(expectedExceptions = DataAccessException.class)
+    void testDeleteNotExistingInstance() {
+        prepareTrainer();
+        preparePokemon();
+
+        doThrow(DataRetrievalFailureException.class).when(dao).delete(pokemon);
+        service.delete(pokemon);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    void testDeleteNullInstance() {
+        doThrow(NullPointerException.class).when(dao).delete((Pokemon) null);
+        service.delete(null);
+    }
+
+    @Test
+    void testLevelPokemonUp() {
+        preparePokemon();
+
+        // saving leveled up
+        when(dao.save(pokemon)).thenReturn(pokemon);
+        pokemon.setLevel(pokemon.getLevel() - 1);
+        service.save(pokemon);
+        pokemon.setLevel(pokemon.getLevel() + 1);
+        verify(dao).save(pokemon);
+    }
+
+    @Test
+    void testGenerateRandomPokemon() {
+
     }
 
     private void prepareTrainer() {
@@ -62,6 +108,13 @@ public class PokemonServiceTest {
         Calendar calendar = Calendar.getInstance();
         calendar.set(1994, Calendar.FEBRUARY, 16);
         trainer.setDateOfBirth(calendar.getTime());
+    }
+
+    private void preparePokemon() {
+        pokemon = new Pokemon(PokemonName.SHELLDER);
+        pokemon.setLevel(13);
+        pokemon.setTrainer(trainer);
+        pokemon.setNickname("Shelly");
     }
 }
 
