da80e4da64607efc9091f2a85c7ab26c90a237af
Matej Poklemba <matejpoklemba@gmail.com>
2016-12-03 17:48:56 +0100

    UserFacadeTest complete

--- service/src/test/java/com/kodemon/service/test/facade/UserFacadeTest.java
+++ service/src/test/java/com/kodemon/service/test/facade/UserFacadeTest.java
@@ -86,6 +86,7 @@ public class UserFacadeTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void registerTest() {
+        when(beanMappingService.mapTo(user, Trainer.class)).thenReturn(trainer);
         when(trainerService.register(trainer, "password")).thenReturn(true);
         boolean result = userFacade.register(user, "password");
         assertThat(result, is(true));
@@ -93,20 +94,34 @@ public class UserFacadeTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void loginTest() {
+        when(beanMappingService.mapTo(user, Trainer.class)).thenReturn(trainer);
         when(trainerService.register(trainer, "password")).thenReturn(true);
-        userFacade.register(user, "password");
+        boolean prerequisite = userFacade.register(user, "password");
+        assertThat(prerequisite, is(true));
 
         UserAuthDTO userAuth = new UserAuthDTO();
         userAuth.setUserName(trainer.getUserName());
         userAuth.setPwdHash(trainer.getPwdHash());
 
-        when(trainerService.login(trainer.getUserName(), "password")).thenReturn(true);
+        when(trainerService.login(userAuth.getUserName(), userAuth.getPwdHash())).thenReturn(true);
         boolean result = userFacade.login(userAuth);
         assertThat(result, is(true));
     }
 
     @Test
-     public void listAllUsers() {
+    public void findUserByUserNameTest() {
+        List<Trainer> trainerListE = new ArrayList<>();
+        trainerListE.add(trainer);
+        List<UserDTO> trainerList = new ArrayList<>();
+        trainerList.add(user);
+        when(trainerService.findByUserName(trainer.getUserName())).thenReturn(trainerListE);
+        when(beanMappingService.mapTo(trainerListE, UserDTO.class)).thenReturn(trainerList);
+        List<UserDTO> result = userFacade.findUserByUserName(user.getUserName());
+        assertThat(result.size(), is(1));
+    }
+
+    @Test
+    public void findAllUsersTest() {
         List<Trainer> allTrainersE = new ArrayList<>();
         allTrainersE.add(trainer);
         allTrainersE.add(trainer2);
