0f2c2a7bffbd7f66cf3841a7e5ea65013657ac50
Oliver Roch <wowflexor@gmail.com>
2016-12-11 16:32:23 +0100

    Added logs

--- sample-data/src/main/java/com/kodemon/sampledata/SampleDataConfig.java
+++ sample-data/src/main/java/com/kodemon/sampledata/SampleDataConfig.java
@@ -1,6 +1,9 @@
 package com.kodemon.sampledata;
 
 import com.kodemon.service.config.ServiceConfig;
+import javassist.tools.reflect.Sample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -19,8 +22,11 @@ public class SampleDataConfig {
     @Inject
     SampleDataLoadingFacade sampleDataLoadingFacade;
 
+    final static Logger LOG = LoggerFactory.getLogger(SampleDataConfig.class);
+
     @PostConstruct
-    public void dataLoading(){
+    public void dataLoading() {
+        LOG.debug("loading sample data");
         sampleDataLoadingFacade.loadData();
     }
 }
--- spring-mvc/src/main/java/com/kodemon/springmvc/controllers/UserController.java
+++ spring-mvc/src/main/java/com/kodemon/springmvc/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.kodemon.springmvc.controllers;
 import com.kodemon.api.dto.UserAuthDTO;
 import com.kodemon.api.dto.UserDTO;
 import com.kodemon.api.facade.UserFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,8 @@ import java.util.Collection;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    final static Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Inject
     private UserFacade userFacade;
@@ -48,7 +52,14 @@ public class UserController {
      */
     @RequestMapping(value = "/detail/{username}", method = RequestMethod.GET)
     public String detail(@PathVariable String username, Model model) {
-        model.addAttribute("trainer", userFacade.findUserByUserName(username).iterator().next());
+        Collection<UserDTO> user = userFacade.findUserByUserName(username);
+        if(user.isEmpty()) {
+            LOG.warn("No trainer with such username found");
+            model.addAttribute("alert_warning", "No trainer with such username found");
+            model.addAttribute("users", userFacade.findAllUsers());
+            return "/user/list";
+        }
+        model.addAttribute("trainer", user.iterator().next());
         return "user/detail";
     }
 
@@ -63,6 +74,7 @@ public class UserController {
     public String find(@RequestParam String username, Model model) {
         Collection<UserDTO> user = userFacade.findUserByUserName(username);
         if(user.isEmpty()) {
+            LOG.warn("No trainer with such username found");
             model.addAttribute("alert_warning", "No trainer with such username found");
             model.addAttribute("users", userFacade.findAllUsers());
             return "/user/list";
@@ -71,6 +83,14 @@ public class UserController {
         return "/user/detail";
     }
 
+    /**
+     *
+     * @param username of trainer to be logged in
+     * @param password of trainer to be logged in
+     * @param r servlet request to create session
+     * @param model data to display
+     * @return JSP page name
+     */
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestParam String username, @RequestParam String password, ServletRequest r, Model model) {
         UserAuthDTO userAuthDTO = new UserAuthDTO();
